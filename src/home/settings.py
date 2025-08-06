548a3ca7323c0ddf06c3968e0312fb91be16ab32
Diwash <diwashshrestha108@gmail.com>
2025-08-06 16:12:09 +0545

    adding raiway custom domain

--- src/home/settings.py
+++ src/home/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = config("DJANGO_SECRET_KEY", cast=str)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DJANGO_DEBUG", cast=bool, default=False)
 
-ALLOWED_HOSTS = [".railway.app"]
+ALLOWED_HOSTS = [".railway.app","django-next-backend-production.up.railway.app"]
+
+if DEBUG:
+  ALLOWED_HOSTS=["*"]
 
 CSRF_TRUSTED_ORIGINS = [
   "http://*.railway.app",
